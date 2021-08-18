8d0033ebd12a032c6dc0bd728b499463a9513448
Sergey Shuktomov <s_shuktomov@mail.ru>
2021-08-18 21:57:20 +0300

    Initial commit

---
+++ tz.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import shutil
+import argparse
+from bs4 import BeautifulSoup
+
+
+class FileManager:
+
+    def __init__(self, config):
+        self.config = config
+
+    def _parser(self):
+        data = BeautifulSoup(self.config.read(), 'lxml')
+        return data.find_all('file')
+
+    def copy_files(self):
+        files = self._parser()
+        for file in files:
+            src = r'{}'.format(file.get('source_path'))
+            dst = r'{}'.format(file.get('destination_path'))
+            fnm = r'{}'.format(file.get('file_name'))
+            sep = os.path.sep
+            sf = src + sep + fnm if src else fnm
+            df = dst + sep + fnm if dst else fnm
+            print(sf, df)
+            try:
+                shutil.copyfile(sf, df)
+            except OSError:
+                print(f'The destination location of file "{fnm}" is not writable')
+            except shutil.SameFileError:
+                print(f'Source and destination are the same file "{fnm}"')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'config',
+        type=argparse.FileType(
+            mode='r',
+            encoding='utf-8',
+        ),
+        help='Enter the path to the config file',
+    )
+    args = parser.parse_args(sys.argv[1:])
+    fm = FileManager(args.config)
+    fm.copy_files()
